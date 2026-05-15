0aa1fa832a163a15d0435df061e6418375628841
tesfa-rel <Tesfamariam.Teklemariam@relevancelab.com>
2026-05-15 17:20:34 +0300

    fix: import backend src directly, merge deps to root

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import app from './dist/index';
+import app from '../backend/src/app';
 
-export default app;
+export default function handler(req: any, res: any) {
+  return (app as any)(req, res);
+}
